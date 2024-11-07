f3d9985e87324466be8e0f5de39b40bfb891f391
Kaue Santoja <shinjiiraki@gmail.com>
2024-11-07 19:45:33 +0100

    Added base object collection

---
+++ src/Spice/Collections/BaseCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Magnetu\Spice\Collections;
+
+use IteratorAggregate;
+use Traversable;
+
+abstract class BaseCollection implements IteratorAggregate
+{
+    public function __construct(protected array $list)
+    {
+    }
+
+    public function add(object $item): void
+    {
+        $this->list[] = $item;
+    }
+
+    #[\Override] public function getIterator(): Traversable
+    {
+        return new \ArrayIterator($this->list);
+    }
+
+    abstract public function toArray(): array;
+}
