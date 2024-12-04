e734476acdaa71245b9e41cfb3a04945f942a0f3
Kaue Santoja <shinjiiraki@gmail.com>
2024-12-04 10:30:00 +0100

    Adding option to force +

--- src/Util/PhoneSanitizer.php
+++ src/Util/PhoneSanitizer.php
@@ -6,14 +6,15 @@ namespace Magnetu\Spice\Util;
 class PhoneSanitizer
 {
 
-    public function sanitize(string $phone): string
+    public function sanitize(string $phone, bool $forcePlus = true): string
     {
         $count = substr_count($phone, '+');
         $phone = preg_replace('/([^0-9])/', '', $phone);
-        if ($count > 0) {
+        if ($forcePlus || $count > 0) {
             $phone = '+'.$phone;
         }
 
         return $phone;
     }
+
 }
